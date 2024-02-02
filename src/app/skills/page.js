46a70255cab892253e78d87b2290aa4718f699b6
Shyam1806 <62296718+Shyam1806@users.noreply.github.com>
2024-02-02 20:40:33 +0530

    "internal changes"

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -12,7 +12,26 @@ function skills() {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 2
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <>
